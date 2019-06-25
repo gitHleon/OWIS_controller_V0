3713e2da6ebdb6d1917c354b1589c3fcf55f316e
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-06-25 13:01:45 +0200

    Update owis_controller.cpp

--- owis_controller.cpp
+++ owis_controller.cpp
@@ -31,15 +31,16 @@ ui->label_stage_state_Z->setText("Z Stage: Off");
 
  connect(ui->joyNegButton_X, SIGNAL(pressed()), this, SLOT(virtualJoy_X()));
  connect(ui->joyPosButton_X, SIGNAL(pressed()), this, SLOT(virtualJoy_X()));
-
  connect(ui->joyNegButton_X, &QPushButton::released, this, &OWIS_controller::stopX);
  connect(ui->joyPosButton_X, &QPushButton::released, this, &OWIS_controller::stopX);
-
  connect(ui->joyNegButton_Y, SIGNAL(pressed()), this, SLOT(virtualJoy_Y()));
  connect(ui->joyPosButton_Y, SIGNAL(pressed()), this, SLOT(virtualJoy_Y()));
-
  connect(ui->joyNegButton_Y, &QPushButton::released, this, &OWIS_controller::stopY);
  connect(ui->joyPosButton_Y, &QPushButton::released, this, &OWIS_controller::stopY);
+ connect(ui->joyNegButton_Z, SIGNAL(pressed()), this, SLOT(virtualJoy_Z()));
+ connect(ui->joyPosButton_Z, SIGNAL(pressed()), this, SLOT(virtualJoy_Z()));
+ connect(ui->joyNegButton_Z, &QPushButton::released, this, &OWIS_controller::stopZ);
+ connect(ui->joyPosButton_Z, &QPushButton::released, this, &OWIS_controller::stopZ);
 
 }
 
