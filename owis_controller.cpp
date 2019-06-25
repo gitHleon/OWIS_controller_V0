1353a808ac0d7c781176d3a29a95097e6c0efacf
gitHleon <45692954+gitHleon@users.noreply.github.com>
2019-06-25 14:05:45 +0200

    Update owis_controller.cpp

--- owis_controller.cpp
+++ owis_controller.cpp
@@ -8,6 +8,7 @@
 #include <QTimer>
 #include <iostream>
 #include <QPushButton>
+#include <QJoysticks.h>
 
 OWIS_controller::OWIS_controller(QWidget *parent) :
     QMainWindow(parent),
@@ -15,6 +16,8 @@ OWIS_controller::OWIS_controller(QWidget *parent) :
 {
     std::cout<<"ok!! "<<std::endl;
     ui->setupUi(this);
+
+ QJoysticks* J_instance = QJoysticks::getInstance();
 
     QTimer *timer = new QTimer(this);
     connect(timer, SIGNAL(timeout()), this, SLOT(updatePositions_X()));
@@ -42,9 +45,51 @@ ui->label_stage_state_Z->setText("Z Stage: Off");
  connect(ui->joyNegButton_Z, &QPushButton::released, this, &OWIS_controller::stopZ);
  connect(ui->joyPosButton_Z, &QPushButton::released, this, &OWIS_controller::stopZ);
 
+// conexión con el Joystick //
+
+    int J_number = J_instance->count();
+    std::cout<<" J_numebr :"<<J_number<<std::endl;
+    QStringList J_list = J_instance->deviceNames();
+    J_number = J_list.size();
+    std::cout<<" J_size :"<<J_number<<std::endl;
+    int J_buttons = J_instance->getNumButtons(0);
+    std::cout<<" J_buttons :"<<J_buttons<<std::endl;
+    //connect(J_instance,SIGNAL(buttonChanged(int,int,bool)), this, SLOT(J_translator(int,int,bool)));
+    connect(J_instance,SIGNAL(axisChanged(int,int,double)), this, SLOT(J_axes_translator(int,int,double)));
+    //connect(this,SIGNAL(Run_focus_signal()), this, SLOT(createTemplate_F()));
+
+
 }
 
 OWIS_controller::~OWIS_controller()
 {
     delete ui;
+}
+
+
+void OWIS_controller::J_axes_translator(int index, int axis, double value)
+{
+    const double threshold = 0.15;
+    if(index != 0 )
+        return; //I want only the main joystick to work
+    if(axis == 0 && (value > threshold))
+        runX(+1);
+    else if(axis == 0 && (value < -threshold))
+        runX(-1);
+    else if(axis == 0 && ((value < threshold) || (value > -threshold)))
+        stopX();
+    else if(axis == 1 && (value > threshold))
+        runY(+1);
+    else if(axis == 1 && (value < -threshold))
+        runY(-1);
+    else if(axis == 1 && ((value < threshold) || (value > -threshold)))
+        stopY();
+    else if(axis == 2 && (value > threshold))
+        runZ(+1);
+    else if(axis == 2 && (value < -threshold))
+        runZ(-1);
+    else if(axis == 2 && ((value < threshold) || (value > -threshold)))
+        stopZ();
+
+
 }
